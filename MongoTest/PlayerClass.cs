fe334e956cf03c3e265da05e167c34db182f7306
Kalle Lindberg <kalle.lindberg@iths.se>
2026-09-24 23:17:09 +0200

    Remove MongoTest from solution

--- MongoTest/PlayerClass.cs
+++
@@ -1,9 +0,0 @@
-﻿using MongoDB.Bson;
-
-public class PlayerClass
-{
-    public ObjectId Id { get; set; }
-    public string Name { get; set; } = "";
-    public int StartHp { get; set; }
-    public char Symbol { get; set; }
-}
--- MongoTest/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿var repo = new GameRepository();
-await repo.SeedClassesAsync();
-
-foreach (var c in await repo.GetClassesAsync())
-    Console.WriteLine($"{c.Name} ({c.StartHp} HP)");
